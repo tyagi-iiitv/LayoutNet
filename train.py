ace8520e0b04558d4f2a8d89d7d1407b71bcfeca
a_tyagi <anjul.ten@gmail.com>
2020-10-14 18:44:33 -0400

    Updated dataset path for colab

--- train.py
+++ train.py
@@ -1,9 +1,8 @@
 import config
 import trainer_step
 
-
 def main():
-    cfg = config.Config(filenamequeue="./dataset/layout_1205.tfrecords")
+    cfg = config.Config(filenamequeue="drive/My Drive/LayoutNet/dataset/layout_1205.tfrecords")
     t = trainer_step.Trainer(cfg)
     t.fit()
 
