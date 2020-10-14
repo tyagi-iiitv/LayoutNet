f9e2feab061431cd1a2d13c4f2581e8f5b7c3c9c
a_tyagi <anjul.ten@gmail.com>
2020-10-14 18:44:09 -0400

    Updated the dataset path for colab

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
 class Config(object):
     def __init__(self, **kwargs):
         # configuration for building the network
+        self.data_dir = os.path.join('drive','My Drive','LayoutNet')
         self.y_dim = kwargs.get("y_dim", 6)
         self.tr_dim = kwargs.get("tr_dim", 7)
         self.ir_dim = kwargs.get("ir_dim", 10)
@@ -11,13 +13,13 @@ class Config(object):
         self.beta1 = kwargs.get("beta1", 0.5)
         # configuration for the supervisor
         self.logdir = kwargs.get("logdir", "./log")
-        self.sampledir = kwargs.get("sampledir", "./example")
+        self.sampledir = kwargs.get("sampledir", os.path.join(self.data_dir, "example"))
         self.max_steps = kwargs.get("max_steps", 30000)
         self.sample_every_n_steps = kwargs.get("sample_every_n_steps", 100)
         self.summary_every_n_steps = kwargs.get("summary_every_n_steps", 1)
         self.save_model_secs = kwargs.get("save_model_secs", 120)
         self.checkpoint_basename = kwargs.get("checkpoint_basename",
                                               "layout")
-        self.filenamequeue = kwargs.get("filenamequeue", "./dataset/layout_1205.tfrecords")
+        self.filenamequeue = kwargs.get("filenamequeue", os.path.join(self.data_dir, "dataset","layout_1205.tfrecords"))
         self.min_after_dequeue = kwargs.get("min_after_dequeue", 5000)
         self.num_threads = kwargs.get("num_threads", 4)
